175d97129bfeccacc70ea1f60189b0a8c93122bb
TheScorn <mucha446@gmail.com>
2023-05-24 17:03:16 +0200

    skrypt działa(na windowsie) niezależnie od folderu w którym działa terminal

--- gen.py
+++ gen.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import random
-
+import os
 
 def generate_person():
+    '''
+    Generuje krotkę z losowym imieniem i nazwiskiem losowej płci.
+    '''
+    print(os.getcwd())
+    os.chdir(os.path.dirname(os.path.abspath(__file__))) #zmieniamy directory na to z projektem
+    os.chdir('names')
+    
+
     sex = random.randrange(0,2)
     print(f"sex:{sex}")
     if sex == 0:
         #generowanie dla male
         number_name = random.randrange(0,100)
         print(number_name)
-        f = open("names/male_names.txt","r",encoding="utf8")
+        f = open("male_names.txt","r",encoding="utf8")
         for i in range(number_name-1):
             f.readline()
         name = f.readline()
@@ -18,7 +26,7 @@ def generate_person():
 
         number_surname = random.randrange(0,42)
         print(number_surname)
-        f = open("names/male_surnames.txt","r",encoding="utf8")
+        f = open("male_surnames.txt","r",encoding="utf8")
         for i in range(number_surname-1):
             f.readline()
         surname = f.readline()
@@ -30,7 +38,7 @@ def generate_person():
         #generowanie dla female
         number_name = random.randrange(0,100)
         print(number_name)
-        f = open("names/female_names.txt","r",encoding="utf8")
+        f = open(f"female_names.txt","r",encoding="utf8")
         for i in range(number_name-1):
             f.readline()
         name = f.readline()
@@ -39,7 +47,7 @@ def generate_person():
         
         number_surname = random.randrange(0,42)
         print(number_surname)
-        f = open("names/female_surnames.txt","r",encoding="utf8")
+        f = open(f"female_surnames.txt","r",encoding="utf8")
         for i in range(number_surname-1):
             f.readline()
         surname = f.readline()
@@ -47,6 +55,10 @@ def generate_person():
         f.close()
         return(name,surname)
 
-osoba = generate_person()
-print(osoba[0])
-print(osoba[1])
+if __name__ == "__main__":
+    osoba = generate_person()
+    print(osoba[0])
+    print(osoba[1])
+
+    
+
