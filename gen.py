ef34ce040c502c77ec0484a8608587fdeb50c852
TheScorn <mucha446@gmail.com>
2023-05-24 17:10:36 +0200

    działający moduł gen.py z opisem

--- gen.py
+++ gen.py
@@ -5,6 +5,7 @@ import os
 def generate_person():
     '''
     Generuje krotkę z losowym imieniem i nazwiskiem losowej płci.
+    Zwraca imie nazwisko i zmienną Sex która może przyjmować 0 dla mężczyzn i 1 dla kobiet
     '''
     print(os.getcwd())
     os.chdir(os.path.dirname(os.path.abspath(__file__))) #zmieniamy directory na to z projektem
@@ -12,11 +13,9 @@ def generate_person():
     
 
     sex = random.randrange(0,2)
-    print(f"sex:{sex}")
     if sex == 0:
         #generowanie dla male
         number_name = random.randrange(0,100)
-        print(number_name)
         f = open("male_names.txt","r",encoding="utf8")
         for i in range(number_name-1):
             f.readline()
@@ -25,7 +24,6 @@ def generate_person():
         f.close()
 
         number_surname = random.randrange(0,42)
-        print(number_surname)
         f = open("male_surnames.txt","r",encoding="utf8")
         for i in range(number_surname-1):
             f.readline()
@@ -37,7 +35,6 @@ def generate_person():
     else:
         #generowanie dla female
         number_name = random.randrange(0,100)
-        print(number_name)
         f = open(f"female_names.txt","r",encoding="utf8")
         for i in range(number_name-1):
             f.readline()
@@ -46,14 +43,13 @@ def generate_person():
         f.close()
         
         number_surname = random.randrange(0,42)
-        print(number_surname)
-        f = open(f"female_surnames.txt","r",encoding="utf8")
+        f = open("female_surnames.txt","r",encoding="utf8")
         for i in range(number_surname-1):
             f.readline()
         surname = f.readline()
         surname = surname[:-1]
         f.close()
-        return(name,surname)
+        return(name,surname,sex)
 
 if __name__ == "__main__":
     osoba = generate_person()
