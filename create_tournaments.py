a6232131131c39f4b5634ef65215964dcf798f9d
TheScorn <mucha446@gmail.com>
2023-06-08 16:02:45 +0200

    create_tournaments działa

---
+++ create_tournaments.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+def create_tournaments():
+    """
+    funkcja tworząca tabelę customers,
+    na ten moment temporary
+    by nie usuwać danych na serwerze,
+    w ten sposób od szablonu można tworzyć i testować
+    wszystkie funkcje tworzące tabele,
+    potem wystarczy zaimportować i odpalić wszystkie na raz.
+    """
+    cs = con.cursor()
+    table = "CREATE OR REPLACE TEMPORARY TABLE tournaments(tournament_id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,game_id SMALLINT NOT NULL,start_date DATE NOT NULL,end_date DATE NOT NULL,prize DECIMAL UNSIGNED DEFAULT NULL)"
+    cs.execute(table)
+    cs.fetchall()
+    
+    alter1 = "ALTER TABLE tournaments ADD CONSTRAINT start_date_in_bounds CHECK (start_date <= '2022-12-01')"
+    alter2 = "ALTER TABLE tournaments ADD CONSTRAINT end_date_in_bounds CHECK (DATE(end_date) > DATE(start_date))"
+    
+    cs.execute(alter1)
+    cs.execute(alter2)
+    
+
+if __name__ == "__main__":
+    import time
+    import datetime
+    import mysql.connector
+    con = mysql.connector.connect(
+            host = "giniewicz.it",
+            user = "team04",
+            password = "te@m0a",
+            database = "team04",
+            )
+    if con:
+        print("connected")
+    else:
+        print("connection error")
+    create_tournaments()
+    ##########
+    #test czy działa wstawiając wartość i odczytując
+    cs = con.cursor()
+    insert = "INSERT INTO tournaments (game_id, start_date, end_date, prize) VALUES (%s, %s, %s, %s)"
+
+    
+    date1 = datetime.datetime(2021,6,14)
+    print(date1)
+    date1_formatted = date1.strftime('%Y-%m-%d')
+    print(date1)
+    date2 = datetime.datetime(2021,6,23)
+    date2_formatted = date2.strftime('%Y-%m-%d')
+
+    val = (4,date1_formatted,date2_formatted, 350)
+    cs.execute(insert,val)
+    con.commit()
+    cs.fetchall()
+
+    cs.execute("SELECT * FROM tournaments")
+    
+    for i in cs:
+        print(i)
+
+    con.close()
--- test.py
+++ test.py
@@ -15,34 +15,20 @@ def main():
         print("connected")
     else:
         print("connection error")
-    '''
+
     cursor = con.cursor()
     cursor.execute("CREATE TEMPORARY TABLE IF NOT EXISTS test(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,data DATE NOT NULL)")
     cursor.fetchall()
     random_date = date_gen()
     #random_date = str(random_date)
-    sql_command = ("INSERT INTO test (data) VALUES ({})".format(random_date))
-    cursor.execute(sql_command)
+    sql_command = "INSERT INTO test (data) VALUES (%s)"
+    val = [random_date]
+    cursor.execute(sql_command,val)
     con.commit()
     cursor.fetchall()
     cursor.execute("SELECT data FROM test")
-    '''
-    cursor = con.cursor()
-    cursor.execute("CREATE TEMPORARY TABLE IF NOT EXISTS test(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,imie VARCHAR(20) NOT NULL,nazwisko VARCHAR(25) NOT NULL)")
-    cursor.fetchall()
-    
-    person = generate_person()
-    sql_command2 = "INSERT INTO test (imie, nazwisko) VALUES (%s,%s)"
-    val = (person[0], person[1])
-    cursor.execute(sql_command2,val)
-    con.commit()
-    cursor.fetchall()
-    
-    sql_command2 = "SELECT imie, nazwisko FROM test"
-    cursor.execute(sql_command2)
+
     for i in cursor:
         print(i)
-    con.close()
-    
 if __name__ == "__main__":
     main()
