4bd19cc526f22bb3b30b6bf7dac566befba651e3
dive1230000 <dive1230000@gmail.com>
2023-06-11 22:23:26 +0200

    losuj_payment i losuj_rental

---
+++ generators/losuj_payment.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import math
+from scipy.stats import arcsine
+import numpy as np
+from losuj_rental import losuj_rental
+import random
+from datetime import datetime, timedelta
+
+dane = losuj_rental(1500,2000)
+
+def last_rental_id(dane, wartosc):
+    ostania_data = datetime(2000, 1, 1).date()
+    wynik = 0
+    for i in dane:
+        if i[5] > ostania_data and i[1] == wartosc :
+            ostania_data = i[5]
+            wynik = i[0]
+    return wynik, ostania_data
+
+
+
+def wywnioskuj_dane(dane):
+    wynik = {}
+    dane_nowe = []
+    for wartosci in dane:
+        dane_nowe.append([wartosci[0],(wartosci[4] - wartosci[3]).days])
+    for wartosci in dane_nowe:
+        if wartosci[0] in wynik:
+            wynik[wartosci[0]] += wartosci[1] 
+        else:
+            wynik[wartosci[0]] = wartosci[1]
+    return dane_nowe,wynik
+
+
+def znajdz_najblizsza_wartosc(lista, zadana_wartosc):
+    return min(lista, key=lambda x: abs(x - zadana_wartosc))
+
+def znajdz_klucze_wedlug_wartosci(slowo, wartosc):
+    klucze = []
+    for klucz, wart in slowo.items():
+        if wart == wartosc:
+            klucze.append(klucz)
+    return klucze
+
+
+def losuj_do_kupna(dane):
+    do_kupna = []
+    wynik = []
+    praca = wywnioskuj_dane(dane)[1]
+    lista = list(praca.values())
+    posortowana_lista = sorted(lista)
+    a = min(posortowana_lista)
+    b = max(posortowana_lista)
+    sample = arcsine.rvs(a, b, size = math.floor(0.3 * len(posortowana_lista)))
+    for x in sample:
+        do_kupna.append(znajdz_najblizsza_wartosc(posortowana_lista, x))
+    for x in do_kupna:
+        wynik.append(znajdz_klucze_wedlug_wartosci(praca,x))
+    wynik = list(set(sum(wynik, [])))
+    return wynik
+
+
+def znizka(dni):
+    return dni 
+
+def losuj_payment(dane):
+    wynik = []
+    dni = wywnioskuj_dane(dane)[1]
+    numerki = losuj_do_kupna(dane)
+    for i in range(0,len(dane)):
+        dane[i].insert(0, i+1)
+    for krok in dane:
+        wynik.append([krok[0],krok[1],krok[2],krok[3],krok[5],(krok[5]-krok[4]).days])
+    for krok in wynik:
+        if krok[1] in numerki:
+            pomoc = last_rental_id(dane, krok[1])
+            if random.randint(0, 1) == 1:
+                c = pomoc[1]
+            else:
+                c = pomoc[1] + timedelta(days = random.randint(1, 50))
+            krok[5] = znizka(dni[krok[1]])
+            wynik.append([f"last rental_id {pomoc[0]}",random.randint(1, 301),random.randint(1, 5),c,znizka(dni[krok[1]])])
+            numerki.remove(krok[1])
+        elif not isinstance(krok[0], int):
+            pass 
+        krok.remove(krok[1])
+    return(wynik)
+
+if __name__ == "__main__":
+    print(losuj_payment(dane))
---
+++ generators/losuj_rental.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import random
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+def znajdz_duplikaty(lista):
+    duplikaty = []
+    for i in range(len(lista)):
+        if lista.count(lista[i]) > 1 and lista[i] not in duplikaty:
+            duplikaty.append(lista[i])
+    return duplikaty
+
+def zlicz_powtorzenia(lista):
+    zliczenia = {}
+    for element in lista:
+        if element in zliczenia:
+            zliczenia[element] += 1
+        else:
+            zliczenia[element] = 1
+    return zliczenia
+
+def spradz_czy_przekroj_pusty(daty):
+    daty = sorted(daty, key=lambda x: x[0])
+    for i in range(len(daty)):
+        roznica = relativedelta(days = 0)
+        for k in range(len(daty)):
+            if k <= i:
+                continue
+            if daty[i][1] > daty[k][0]:
+                roznica += relativedelta(days = (daty[i][1] - daty[k][0]).days) 
+                daty[k][0] = daty[k][0] +  (roznica + relativedelta(days = 1))
+                daty[k][1] = daty[k][1] +  (roznica + relativedelta(days = 1))
+    return daty
+
+
+
+
+def losuj_daty(ile_razy):
+    start_date = datetime(2020, 1, 1)
+    start_date = start_date.date() 
+    wynik = []
+    for i in range(ile_razy):
+        rok = random.randint(0,2)
+        s_date = start_date + relativedelta(years=rok) + relativedelta(months=random.randint(1, 12)) + relativedelta(days=random.randint(1, 28))
+        e_date = s_date + timedelta(days=random.randint(2, 60))
+        wynik.append([s_date, e_date])
+    return(wynik)
+
+
+def losuj_rental(p, k):
+    wynik = []
+    liczba_wypozyczen = random.randint(p, k)
+    numerki = random.choices(range(1, 401), k=liczba_wypozyczen)
+    slownik = zlicz_powtorzenia(numerki)
+    for klucz, wartosc in slownik.items():
+        slownik[klucz] = spradz_czy_przekroj_pusty(losuj_daty(wartosc))
+        for x in slownik[klucz]:
+            wynik.append([klucz, x[0], x[1]])
+    for w in wynik:
+        w.append(random.randint(1,300))
+        w.append(random.randint(1,5))
+        w[1], w[3] = w[3], w[1]
+        w[2], w[4] = w[4], w[2]
+    return wynik
+
+if __name__ == "__main__":
+    dane = losuj_rental(4000,12000)
+    print(dane)
+
+
